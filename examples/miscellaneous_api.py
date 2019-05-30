57f2453880d331d340cc7058851ce248efaffe27
NostrumBioDiscovery <daniel.soler@nostrumbiodiscovery.com>
2019-05-30 04:10:19 +0200

    example api use

---
+++ examples/miscellaneous_api.py
@@ -0,0 +1,17 @@
+# Load Molecule and look for commound compound and its different
+# conformations in the different sdf files
+
+from rdkit import Chem
+import glob
+from miscellaneous import search_molec_sdfs as sd
+
+output="output.sdf"
+sdf_files = glob.glob("../tests/data/sdf_file*/test_*.sdf")
+mols_in_all_sdfs = sd.search_molecule_in_all_sdf(sdf_files)
+
+#Output molecules and all conformations
+w = Chem.SDWriter(output)
+for m, conformations in mols_in_all_sdfs:
+    w.write(m)
+    for conformation in conformations:
+        w.write(conformation)
